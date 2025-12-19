331cf36a080eeeb3f97ee71de68e3efc9044c822
prungthan <prungthan@hotmail.com>
2025-12-19 13:14:37 +0700

    Update etl_pipeline.py

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -139,7 +139,7 @@ def deploy_to_database(fact_table, dim_tables):
     print("\n🚀 Deploying to Database...")
     
     # Database configuration
-    server = "mssql.minddatatech.com"
+    server = "10.18.31.211"
     database = 'TestDB'
     username = 'SA'
     password = os.getenv('DB_PASSWORD', 'Passw0rd123456')
