905cca2800c513bfad26848e62be5ac5373b1828
prungthan <prungthan@hotmail.com>
2025-12-19 11:55:53 +0700

    Add files via upload

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -139,7 +139,7 @@ def deploy_to_database(fact_table, dim_tables):
     print("\n🚀 Deploying to Database...")
     
     # Database configuration
-    server = "10.18.31.211"
+    server = "mssql.minddatatech.com"
     database = 'TestDB'
     username = 'SA'
     password = os.getenv('DB_PASSWORD', 'Passw0rd123456')
